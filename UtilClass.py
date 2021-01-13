398b8ebe41f8621974e840cc628625d2ea7178f0
Eric Näser <77387312+eric-official@users.noreply.github.com>
2021-01-13 15:52:14 +0100

    Update UtilClass.py

--- UtilClass.py
+++ UtilClass.py
@@ -3,9 +3,9 @@ import spotipy.util as util
 
 def create_token(username, scope):  # scope='user-library-read'
 
-    client_id = '9e081501ea5b4564bfd7e424762e756a'
-    client_secret = '04150d528cd5471dac96be12d55dcac7'
-    redirect_uri = 'http://localhost:8888/callback'
+    client_id = 'YOUR_CLIENT_ID'
+    client_secret = 'YOUR_CLIENT_SECRET'
+    redirect_uri = 'YOUR_REDIRECT_URI'
     token = util.prompt_for_user_token(username=username,
                                        scope=scope,
                                        client_id=client_id,
